5117364cebeaa1e6a67538f631de64d666cb597f
Marius De Allie <spectre4300s@gmail.com>
2019-05-12 17:45:08 -0400

    Modify enemy update method to move enemy objects within gameboard boundary.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+// Enemies our player must avoid
+function Enemy() {
+    this.x = 0;
+    this.y = 0;
+    this.sprite = 'images/enemy-bug.png';
+  };
+
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  Enemy.prototype.update = function(dt) {
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+      if(this.x < 505) {
+        this.x += 200 * dt;
+      }
+  };
+
+  // Draw the enemy on the screen, required method for game
+  Enemy.prototype.render = function() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
 class Hero {
   constructor() {
     this.x = 202;
@@ -24,31 +50,6 @@ class Hero {
     }
   }
 }
-
-// Enemies our player must avoid
-function Enemy() {
-    this.x = 0;
-    this.y = 0;
-    this.sprite = 'images/enemy-bug.png';
-  };
-
-  // Update the enemy's position, required method for game
-  // Parameter: dt, a time delta between ticks
-  Enemy.prototype.update = function(dt) {
-      // You should multiply any movement by the dt parameter
-      // which will ensure the game runs at the same speed for
-      // all computers.
-  };
-
-  // Draw the enemy on the screen, required method for game
-  Enemy.prototype.render = function() {
-      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
