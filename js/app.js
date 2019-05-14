e5f946a6eda222927d1b8fdfdb679811fecb89c7
Marius De Allie <spectre4300s@gmail.com>
2019-05-14 17:59:57 -0400

    Add comments to app.js, update code to udacity js styleguide specs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-function Enemy(x, y, speed) {
+function Enemy(x, y, speed) { //Enemy constructor function declaration
     this.x = x;
     this.y = y + 55;
     this.sprite = 'images/enemy-bug.png';
@@ -8,15 +8,17 @@ function Enemy(x, y, speed) {
 
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
-  Enemy.prototype.update = function(dt) {
+  Enemy.prototype.update = function(dt) { //Enemy update prototype method
       // You should multiply any movement by the dt parameter
       // which will ensure the game runs at the same speed for
       // all computers.
-      if(this.x < 505) {
-        this.x += this.enemySpeed * dt;
+
+      if(this.x < 505) { //Check if Enemy x coordinate is within game boundaries
+        this.x += this.enemySpeed * dt; //update enemy x coordinate
       }
       else {
-        this.x = -1;
+        this.x = -1; //if x coordinate is outside boundary then set enemy just
+        // slightly offscreen to the left
       }
   };
 
@@ -25,9 +27,7 @@ function Enemy(x, y, speed) {
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Hero class
 class Hero {
   constructor() {
     this.x = 202;
@@ -40,52 +40,58 @@ class Hero {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
+  /* Function to handle player keyboard inputs and update player x and y
+  property values based keyboard input recieved */
   handleInput(input) {
-    if (input === 'left' && this.x > 0) {
+    if (input === 'left' && this.x > 0) { //check player is within left boundary
       this.x -= 101;
     }
-    else if (input === 'up' && this.y > 83) {
-      this.y -= 83;
+    else if (input === 'up' && this.y > 83) { //check player is within upper
+      this.y -= 83;                           //boundary
     }
-    else if (input === 'right' && this.x < 404) {
-      this.x += 101;
+    else if (input === 'right' && this.x < 404) { //check player is within right
+      this.x += 101;                              //boundary
     }
-    else if (input === 'down' && this.y < 387) {
-      this.y += 83;
+    else if (input === 'down' && this.y < 387) { //check player is within lower
+      this.y += 83;                             //boundary
     }
   }
 
+  // Function to reset player x and y coordinates to starting position on board.
   reset() {
     this.x = 202;
     this.y = 387;
   }
 
+  // Function to detect player and enemy object collisions
   update() {
-    for(let currentEnemy of allEnemies) {
+    for(let currentEnemy of allEnemies) { // loop through enemies array
+      // compare each enemy in array xy coordinates to player xy coordinates
       if (this.y === currentEnemy.y && (currentEnemy.x + 50.5 > this.x &&
         currentEnemy.x < this.x + 50.5)) {
-          this.reset();
+          this.reset(); // call reset method on player if collision occurs
         }
     }
-    if(this.y === 55) {
-      this.win = true;
+    if(this.y === 55) { // check player's y coordinate for game win condition
+      this.win = true; // set player win property value to true
     }
   }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const player = new Hero();
-let allEnemies = [];
+
+const player = new Hero(); // Instantiate Hero your object
+let allEnemies = []; // Declare empty array to hold enemy objects
+// Instantiate enemy objects with x, y and speed arguments passed into constructor
 const enemy1= new Enemy(-101, 83, 300);
 const enemy2= new Enemy(-202, 0, 225);
 const enemy3= new Enemy(-202, 166, 350);
 const enemy4= new Enemy(303, 0, 200);
+// Append enemy objects to allEnemies array
 allEnemies.push(enemy1, enemy2, enemy3, enemy4);
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     const allowedKeys = {
         37: 'left',
@@ -94,5 +100,7 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
+    /* Call handleInput method on player object passing in keypresses from event
+    Listener */
     player.handleInput(allowedKeys[e.keyCode]);
 });
